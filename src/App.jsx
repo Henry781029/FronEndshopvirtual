b78ee98b95e9f5eeb6c65cab6c109887ffc518d3
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-10-24 02:36:02 -0500

    se ajustan componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Button } from '@mui/material';
+import loginUser from './Authenticate/Login';
+import layoutUser from './Component/Layout';
 
 export const App = () => {
     return (
-        <div>
-            hola, mundo
-        </div>
+        <Router>
+            <Suspense
+            fallback = {<p>loading...</p>}
+            >
+                <Switch>
+                    <Route exact path='/' name='loginUser'><loginUser />
+                    </Route>
+                    <Route exact path='/app' name='layout'><layoutUser />
+                    </Route>
+                    <Button
+                    variant= "contained"
+                    >MATERIAL UI</Button>
+                </Switch>
+            </Suspense>
+        </Router>
     )
 }
 
---
+++ src/Component/Layout/layoutUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const layoutUser = () => {
+    return (
+        <div>
+            hola soy layout
+        </div>
+    )
+}
+
+
+export default layoutUser;
